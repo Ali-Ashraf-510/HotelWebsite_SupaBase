59f858e6ca7c82b2c2762c994da096e9b5cec85a
Ali Ashraf‬ <147933300+Ali-Ashraf-510@users.noreply.github.com>
2025-04-24 22:30:22 +0200

    Refactor Navbar component for improved layout and styling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,28 +4,37 @@ import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'; // لو حابب تستخدم أيقونة الموبايل
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatToolbarModule, MatButtonModule],
+  imports: [CommonModule, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  user: any;
+  user: any = null;
+  isMenuOpen: boolean = false;
 
-  constructor(private supabaseService: SupabaseService, private router: Router) {}
+  constructor(
+    private supabaseService: SupabaseService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.supabaseService.currentUser.subscribe(user => {
       this.user = user;
     });
   }
-  
 
   async signOut() {
     await this.supabaseService.signOut();
+    this.user = null;
     this.router.navigate(['/hotels']);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
